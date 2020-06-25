0fc71c476fcc48ade7b15ed182d77e88b46b2203
sebastianGrajales1992 <sebastian.grajales1992@gmail.com>
2020-06-25 01:38:02 -0500

    practica blog 5

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.6'
 
+gem 'record_tag_helper', '~> 1.0'
 # Annote modals
 gem 'annotate'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+require_relative '../lib/timer'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -11,6 +13,7 @@ module Blog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.middleware.insert_before(Rack::Sendfile, Timer)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
